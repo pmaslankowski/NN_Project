3f80f30038f5a6ee78335cb1527241bccf26d31e
pmaslankowski <pmaslankowski@gmail.com>
2018-01-25 02:59:51 +0100

    Bug in pull_storage.sh script fixed

--- pull_storage.sh
+++ pull_storage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p sketch_rnn/datasets
 mkdir -p sketch_rnn/models
-mkdir -p sketch_rnn/log
+mkdir -p sketch_rnn/logs
 gsutil -m rsync -d -r gs://nn_data/datasets sketch_rnn/datasets
 gsutil -m rsync -d -r gs://nn_data/models sketch_rnn/models
-gsutil -m rsync -d -r gs://nn_data/models sketch_rnn/log
+gsutil -m rsync -d -r gs://nn_data/logs sketch_rnn/logs
